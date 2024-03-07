247d251d536962228be15fa2f1d5e1951ad2e50a
OstapchukAB <electron.anton@gmail.com>
2024-03-07 21:37:12 +0300

    начал реализовывать grid

---
+++ Data/Employee4.cs
@@ -0,0 +1,11 @@
+﻿namespace MyBlazorAppFirst.Data
+{
+    public class Employee4
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public string? Designation { get; set;}
+        public DateOnly DOJ { get; set;}
+        public bool IsActive { get; set;}
+    }
+}
